a36091ba40dcc41b250b9c28bcc43c4d32560d97
Ivanov Dmitry <g.arraw@gmail.com>
2013-02-21 22:44:17 +0400

    lab04

---
+++ lab2/src/ru/spbstu/telematics/ivanov722/lab04/Message.java
@@ -0,0 +1,20 @@
+package ru.spbstu.telematics.ivanov722.lab04;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+	String from;
+	String text;
+	
+	public String getFrom() {
+		return from;
+	}
+	public String getText() {
+		return text;
+	}
+	public Message(String from, String text) {
+		super();
+		this.from = from;
+		this.text = text;
+	}
+}
